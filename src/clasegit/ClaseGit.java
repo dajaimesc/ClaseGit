ae60ad369ec91900ec3ef1ba268c8ea7210a07b3
danieljaimes <danieljaimes@192.168.0.9>
2021-02-18 18:16:55 -0600

    Clase de Git

---
+++ src/clasegit/ClaseGit.java
@@ -0,0 +1,24 @@
+/*
+ * Lenguajes de programación I
+ * Facultad de Ingeniería
+ * Universidad Autónoma de Campeche
+ */
+package clasegit;
+
+/**
+ *
+ * @author danieljaimes
+ */
+public class ClaseGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ejemplo de control de versiones con git");
+        
+        // Cooperemos
+    }
+    
+}
